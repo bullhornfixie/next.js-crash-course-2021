efc6be3e7f45333364934a34a23683f3cc95dd85
Jon Dawson <dawson.jon@hotmail.com>
2021-12-20 22:49:31 +0000

    nested routes with path id

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,9 +1,31 @@
-const article = () => {
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const article = ({ article }) => {
+  // const router = useRouter()
+  // const { id } = router.query
+  console.log(article)
   return (
-    <div>
-      This is an article
-    </div>
+    <>
+      <h1>{article.title}</h1>
+      <h2>This is article {article.id}</h2>
+      <p>{article.body}</p>
+      <br />
+      <Link href='/'>Go Back</Link>
+    </>
   )
 }
 
+// Each time we visit page we call this function and grab the path id 
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+  const article = await res.json()
+
+  return {
+    props: {
+      article
+    }
+  }
+}
+ 
 export default article
